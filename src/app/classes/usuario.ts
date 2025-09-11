f37b17091fcfabc40d941cb686d02d76b15ee81d
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-11 15:01:51 -0300

    add: clase usuario

--- src/app/classes/usuario.ts
+++ src/app/classes/usuario.ts
@@ -1,2 +1,10 @@
 export class Usuario {
+  constructor(
+    public id?: string,
+    public email?: string,
+    public nombre?: string,
+    public apellido?: string,
+    public edad?: number,
+    public fechaRegistro?: Date,
+  ) {}
 }
