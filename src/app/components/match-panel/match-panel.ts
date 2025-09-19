28769b2deae420b5f826a4555f30dddf539884b7
JoaquinCarbonaro <joacolcarbo@gmail.com>
2025-09-19 12:56:22 -0300

    add: estructura ahorcado

---
+++ src/app/components/match-panel/match-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-panel',
+  imports: [],
+  templateUrl: './match-panel.html',
+  styleUrl: './match-panel.css'
+})
+export class MatchPanel {
+
+}
